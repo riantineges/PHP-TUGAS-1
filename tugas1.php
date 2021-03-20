c7116dd224dbc7db2de0a43c1f714ec5b9022aae
riantineges <rian.tineges@gmail.com>
2021-03-20 10:02:35 +0700

    Tugas 1 PHP
    Membuat Daftar Menu dengan menggunakan fungsi output, variabel dan konstanta

---
+++ tugas1.php
@@ -0,0 +1,57 @@
+<?php
+echo "<h2>Daftar Menu</h2>";
+echo "<table>
+<tr>
+<td><li>Ayam Goreng</li></td>
+<td>=</td>
+<td>Rp.13000</td>
+</tr>
+
+<tr>
+<td><li>Ayam Bakar</li></td>
+<td>=</td>
+<td>Rp.15000</td>
+</tr>
+
+<tr>
+<td><li>Ayam Sayur</li></td>
+<td>=</td>
+<td>Rp.13000</td>
+</tr>
+
+<tr>
+<td><li>Tempe Goreng</li></td>
+<td>=</td>
+<td>Rp.1000</td>
+</tr>
+
+<tr>
+<td><li>Tahu Goreng</li></td>
+<td>=</td>
+<td>Rp.1000</td>
+</tr>
+
+<tr>
+<td><li>Soto</li></td>
+<td>=</td>
+<td>Rp.3000</td>
+</tr>
+
+<tr>
+<td><li>Nasi Putih</li></td>
+<td>=</td>
+<td>Rp.5000</td>
+</tr>
+
+<tr>
+<td><li>Es Teh</li></td>
+<td>=</td>
+<td>Rp.4000</td>
+</tr>
+
+<tr>
+<td><li>Es Tebu</li></td>
+<td>=</td>
+<td>Rp.5000</td>
+</tr>
+</table>";
