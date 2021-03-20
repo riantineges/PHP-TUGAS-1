92d9281efd6e52bc5239f552c5e86c133642947a
riantineges <rian.tineges@gmail.com>
2021-03-20 10:15:32 +0700

    PHP-Tugas-1-2
    Membuat list daftar menu versi table

---
+++ tugas1_2.php
@@ -0,0 +1,41 @@
+<?php
+echo "<h1>DAFTAR MENU</h1>";
+echo "
+<table border='1'>
+<tr>
+<td>No</td>
+<td>Nama Menu</td>
+<td>Harga</td>
+</tr>
+
+<tr>
+<td>1.</td>
+<td>Cireng</td>
+<td>Rp 1000</td>
+</tr>
+
+<tr>
+<td>2.</td>
+<td>Es Duren</td>
+<td>Rp 15000</td>
+</tr>
+
+<tr>
+<td>3.</td>
+<td>Es Milo</td>
+<td>Rp 20000</td>
+</tr>
+
+<tr>
+<td>4.</td>
+<td>Es Tesh</td>
+<td>Rp 5000</td>
+</tr>
+
+<tr>
+<td>5.</td>
+<td>Piscok</td>
+<td>Rp 1000</td>
+</tr>
+</table>
+";
